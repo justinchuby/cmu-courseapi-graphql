040d125fdcf08f87ffcaf1a99125396866dec39b
justinchuby <justinchuby@users.noreply.github.com>
2019-02-20 14:17:13 -0500

    Update Graphql schema

--- schema.js
+++ schema.js
@@ -1,20 +1,57 @@
-import { gql, makeExecutableSchema } from "apollo-server-express";
+import { gql, makeExecutableSchema } from 'apollo-server-express'
 
 // The GraphQL schema
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: Author
+  type Course {
+    courseId: String!
+    desc: String
+    name: String
+    notes: String
+    department: String
+    meetings: [Meeting]
+    coreqs: String
+    coreqsObj: Requisite
+    prereqs: String
+    prereqsObj: Requisite
+    semester: String
+    year: Int
+    units: Int
+    rundate: Date
   }
 
-  type Author {
-    books: [Book]
+  type Meeting {
+    courseId: String!
+    name: String
+    times: [EventTime]
+    instructors: [String]
+    type: String
+    semester: String
+    year: Int
+    rundate: Date
+  }
+
+  type EventTime {
+    days: [Int]
+    begin: Int
+    end: Int
+    building: String
+    room: String
+    location: String
+  }
+
+  type Requisite {
+    reqs: [[String]]
+    invert: Boolean
   }
 
   type Query {
-    author: Author
+    course(courseId: String): Course
+
   }
 `
+
+// TODO: See https://graphql.org/learn/schema/#union-types
+// TODO: See https://www.apollographql.com/docs/graphql-tools/scalars.html#Date-as-a-scalar
 
 // A map of functions which return data for the schema.
 const resolvers = {
