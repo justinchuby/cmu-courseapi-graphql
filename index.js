9220903a124f67135b3392eecb95a5be4adb16ab
justinchuby <justinchuby@users.noreply.github.com>
2019-02-20 19:33:35 -0500

    Move resolvers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { schema } from './schema'
--- schema.js
+++ schema.js
@@ -1,8 +1,5 @@
 import { gql, makeExecutableSchema } from 'apollo-server-express'
-
-// Resolver dependency
-import { GraphQLScalarType } from 'graphql'
-import { Kind } from 'graphql/language'
+import { resolvers } from './resolvers'
 
 // The GraphQL schema
 const typeDefs = gql`
@@ -56,42 +53,8 @@ const typeDefs = gql`
 
   type Query {
     course(courseId: String): Course
-    
   }
 `
-
-// TODO: See https://graphql.org/learn/schema/#union-types
-// TODO: See https://www.apollographql.com/docs/graphql-tools/scalars.html#Date-as-a-scalar
-
-// A map of functions which return data for the schema.
-const resolvers = {
-  Date: new GraphQLScalarType({
-    name: 'Date',
-    description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value) // value from the client
-    },
-    serialize(value) {
-      return value.getTime() // value sent to the client
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return new Date(ast.value) // ast value is always in string format
-      }
-      return null
-    },
-  }),
-  // Query: {
-  //   author(parent, args, context, info) {
-  //     return find(authors, { id: args.id });
-  //   }
-  // },
-  // Author: {
-  //   books(author) {
-  //     return filter(books, { author: author.name });
-  //   }
-  // }
-}
 
 export const schema = makeExecutableSchema({
   typeDefs,
