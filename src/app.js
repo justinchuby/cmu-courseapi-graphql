3fd2f11273ee99558f939f8264e38b352f97096e
justinchuby <justinchuby@users.noreply.github.com>
2019-02-24 10:36:22 -0500

    Fix env vars

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { CourseApiServer } from './index'
 
 dotenv.config()
 
-const mongoUri = process.env.MONGO_URI
+const mongoUri = `${process.env.MONGO_URI}/${process.env.DB_NAME}?retryWrites=true`
 const port = process.env.PORT
 const engineApiKey = process.env.ENGINE_API_KEY
 // DEBUG
